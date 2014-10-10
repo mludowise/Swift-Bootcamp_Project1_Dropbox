0e69371e41230e0588ea1377b041a7eb74404f87
mludowise <mludowise@gmail.com>
2014-10-10 00:47:53 -0700

    Got Welcome back button working!
    Hehe… “welcome back”, get it?
    
    Changed the image background to the create account page
    programmatically, so it’s actually using the same ViewController.

--- dropbox/CreateAccountViewController.swift
+++ dropbox/CreateAccountViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class CreateAccountViewController: UIViewController {
 
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
+    // Buttons that the user will click on to simulate filling in the form
+    @IBOutlet weak var formButton: UIButton!
+    @IBOutlet weak var keyboardButton: UIButton!
+    
+    // Buttons that allow user to finish creating their account
+    @IBOutlet weak var createButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    // We'll replace the background image with these to simulate the user filling in the form
+    let signUpImages = ["create1", "create2", "create3", "create4"]
+    private var imageIndex = 0 // Current image we're using from above. Initiate to 0 to represent that the background isn't currently set to any of these.
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,10 +35,24 @@ class CreateAccountViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func onNextScreen(sender: AnyObject) {
+        // If the user clicked the first time, enable the create buttons
+        if (imageIndex == 0) {
+            createButton.enabled = true
+            nextButton.enabled = true
+        }
+        
+        // Set the image to the next in the progression
+        backgroundImage.image = UIImage(named: signUpImages[imageIndex++])
+        
+        // If this is the last screen, disable the buttons that call this function
+        if (imageIndex == signUpImages.count) {
+            formButton.enabled = false
+            keyboardButton.enabled = false
+        }
+    }
+    
     @IBAction func onWelcomeButton(sender: AnyObject) {
-        dismissViewControllerAnimated(true, completion: { () -> Void in
-            self.navigationController?.popToRootViewControllerAnimated(true)
-            return Void()
-        })
+        navigationController?.popToRootViewControllerAnimated(true)
     }
 }
